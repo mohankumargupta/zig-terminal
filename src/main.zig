38bb7499baddd1024671af9cd0baa7fa8d7007af
Mohan Gupta <mohangupta@live.com>
2024-01-30 11:57:40 +1100

    awesome

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,10 @@ const std = @import("std");
 const Terminal = @import("terminal.zig").Terminal;
 
 pub fn main() !void {
-    var terminal = Terminal{};
+    var terminal = Terminal{ .in = std.io.getStdIn(), .out = std.io.getStdOut() };
     terminal.init();
+    try terminal.write("\x1b[1;31mHello\x1b[0m");
+    defer terminal.deinit();
 }
 
 test "simple test" {}
--- src/terminal_windows.zig
+++ src/terminal_windows.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
 
-in: std.fs.File = undefined,
-out: std.fs.File = undefined,
-_console: *UTF8ConsoleOutput = undefined,
+in: std.fs.File,
+out: std.fs.File,
+_consoleCP: *UTF8ConsoleOutput = undefined,
+_consoleMode: u32 = 0,
 
 const Self = @This();
 
@@ -32,9 +33,17 @@ pub fn init(self: *Self) void {
     std.log.info("windows", .{});
     var utf8 = UTF8ConsoleOutput{};
     utf8.init();
-    self._console = &utf8;
+    self._consoleCP = &utf8;
+    const ENABLE_VIRTUAL_TERMINAL_PROCESSING = 4;
+    _ = kernel32.GetConsoleMode(self.in.handle, &self._consoleMode);
+    _ = kernel32.SetConsoleMode(self.out.handle, self._consoleMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
 }
 
 pub fn deinit(self: *Self) void {
-    self._console.deinit();
+    self._consoleCP.deinit();
+    _ = kernel32.SetConsoleMode(self.out.handle, self._consoleMode);
+}
+
+pub fn write(self: *Self, comptime fmt: []const u8) !void {
+    _ = try self.out.write(fmt);
 }
